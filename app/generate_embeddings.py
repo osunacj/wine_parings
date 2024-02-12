b179152d602eb0312f23b1cf70e52e1ec2cecdc8
Carlos Osuna <osunacj@gmail.com>
2024-02-12 11:12:08 +0100

    modify the vocab before generating embeddings

--- app/generate_embeddings.py
+++ app/generate_embeddings.py
@@ -8,8 +8,9 @@ from tqdm import tqdm
 import pickle
 
 from notebooks.helpers.models.embedding_model import PredictionModel
-from notebooks.helpers.prep.utils import get_all_ingredients
+from notebooks.helpers.prep.utils import get_all_ingredients, modify_vocabulary
 from notebooks.helpers.prep.wine_mapping_values import wine_terms_mappings
+from notebooks.helpers.prep.synonmy_replacements import synonyms
 
 core_tastes = [
     "aroma",
@@ -42,24 +43,6 @@ def _random_sample_with_min_count(population, k):
         return random.sample(population, k)
 
 
-def sample_random_sentence_dict(max_sentence_count):
-    food_to_sentences_dict = _generate_food_sentence_dict()
-    # only keep 100 randomly selected sentences
-    food_to_sentences_dict_random_samples = {
-        food: _random_sample_with_min_count(sentences, max_sentence_count)
-        for food, sentences in food_to_sentences_dict.items()
-    }
-    return food_to_sentences_dict_random_samples
-
-
-def _map_ingredients_to_input_ids():
-    model = PredictionModel()
-    ingredient_ids = model.tokenizer.convert_tokens_to_ids(all_ingredients)
-    ingredient_ids_dict = dict(zip(all_ingredients, ingredient_ids))
-
-    return ingredient_ids_dict
-
-
 def _generate_food_sentence_dict():
     wine_reviews, ingredients_in_reviews = get_wine_and_ingredients_reviews()
 
@@ -80,6 +63,43 @@ def _generate_food_sentence_dict():
     return food_to_sentences_dict
 
 
+def _merge_synonmys(food_to_embeddings_dict):
+    synonmy_replacements_path = Path(
+        "./app/notebooks/helpers/prep/synonmy_replacements.py"
+    )
+    if synonmy_replacements_path.exists():
+        synonmy_replacements = synonyms
+
+    else:
+        synonmy_replacements = {}
+
+    # Map synonyms mapping
+    for key in food_to_embeddings_dict.keys():
+        if key in synonmy_replacements:
+            key_to_use = synonmy_replacements[key]
+            food_to_embeddings_dict[key] = food_to_embeddings_dict[key_to_use]
+
+    return food_to_embeddings_dict
+
+
+def sample_random_sentence_dict(max_sentence_count):
+    food_to_sentences_dict = _generate_food_sentence_dict()
+    # only keep 100 randomly selected sentences
+    food_to_sentences_dict_random_samples = {
+        food: _random_sample_with_min_count(sentences, max_sentence_count)
+        for food, sentences in food_to_sentences_dict.items()
+    }
+    return food_to_sentences_dict_random_samples
+
+
+def _map_ingredients_to_input_ids():
+    model = PredictionModel()
+    ingredient_ids = model.tokenizer.convert_tokens_to_ids(all_ingredients)
+    ingredient_ids_dict = dict(zip(all_ingredients, ingredient_ids))
+
+    return ingredient_ids_dict
+
+
 def generate_food_embedding_dict(max_sentence_count=50):
     food_to_embeddings_dict_path = Path(
         f"./app/notebooks/helpers/models/food_embeddings_dict_foodbert.pkl"
@@ -87,6 +107,22 @@ def generate_food_embedding_dict(max_sentence_count=50):
     if food_to_embeddings_dict_path.exists():
         with food_to_embeddings_dict_path.open("rb") as f:
             food_to_embeddings_dict = pickle.load(f)
+
+        # delete keys if we deleted ingredients
+        old_ingredients = set(food_to_embeddings_dict.keys())
+        new_ingredients = set(get_all_ingredients())
+
+        keys_to_delete = old_ingredients.difference(new_ingredients)
+        for key in keys_to_delete:
+            food_to_embeddings_dict.pop(key, None)  # delete key if it exists
+
+        food_to_embeddings_dict = _merge_synonmys(food_to_embeddings_dict)
+
+        with food_to_embeddings_dict_path.open("wb") as f:
+            pickle.dump(
+                food_to_embeddings_dict, f
+            )  # Overwrite dict with cleaned version
+
     else:
         print("\nSampling Random Sentences")
         food_to_sentences_dict_random_samples = sample_random_sentence_dict(
@@ -116,10 +152,22 @@ def generate_food_embedding_dict(max_sentence_count=50):
             k: np.stack(v) for k, v in food_to_embeddings_dict.items() if v
         }
 
+        food_to_embeddings_dict = _merge_synonmys(food_to_embeddings_dict)
+
         with food_to_embeddings_dict_path.open("wb") as f:
             pickle.dump(food_to_embeddings_dict, f)
 
     return food_to_embeddings_dict
+
+
+def get_ingredient_mean_of_embeddings(ingredient):
+    food_to_embeddings_dict = generate_food_embedding_dict()
+    ingredient_embedings = food_to_embeddings_dict.get(ingredient)
+    if ingredient in food_to_embeddings_dict:
+        # Get the average of the embeddings of ingredient
+        mean = np.average(ingredient_embedings, axis=0)
+        return mean
+    return []
 
 
 def get_taste_of_ingredient(ingredient):
@@ -132,7 +180,19 @@ def get_taste_of_ingredient(ingredient):
     return taste
 
 
-def construct_taste_ingredient_embeddings(food_to_embeddings_dict):
+def average_taste_embeddings(dataframe):
+    # pull the average embedding for the wine attribute across all wines.
+    avg_taste_embeddings = dict()
+    for core_taste in core_tastes:
+        # look at the average embedding for a taste, across all wines that have descriptors for that taste
+        review_arrays = dataframe[core_taste + "_embeddings"].dropna()
+        avg_taste_embeddings[core_taste + "_embeddings"] = np.average(review_arrays)
+
+    return avg_taste_embeddings
+
+
+def construct_taste_ingredient_embeddings():
+    # Need to run the fodbert vocab script and a way to merge synonyms
     wine_dataset = pd.read_csv("./app/data/test/reduced_wines.csv")
     descriptors_in_reviews = wine_dataset.loc[:, "descriptors_in_reviews"].to_numpy()
 
@@ -149,17 +209,32 @@ def construct_taste_ingredient_embeddings(food_to_embeddings_dict):
                 taste = get_taste_of_ingredient(ingredient)
                 if taste == core_taste:
                     ingredients.append(ingredient)
-                    embeddings.append(food_to_embeddings_dict.get(ingredient))
+                    embeddings.append(get_ingredient_mean_of_embeddings(ingredient))
+
+            embeddings = [embedding for embedding in embeddings if embedding]
+            if embeddings:
+                constructor[core_taste + "_embeddings"].append(
+                    np.average(embeddings, axis=0)
+                )
+            else:
+                constructor[core_taste + "_embeddings"].append([])
             constructor[core_taste + "_descriptors"].append(ingredients)
-            constructor[core_taste + "_embeddings"].append(embeddings)
 
     embeddings_dataset = pd.DataFrame(constructor, columns=list(constructor.keys()))
-    wine_df = pd.concat([wine_dataset, embeddings_dataset], axis=1)
+
+    return embeddings_dataset
 
 
 def main():
-    food_to_embeddings_dict = generate_food_embedding_dict()
-    construct_taste_ingredient_embeddings(food_to_embeddings_dict)
+    modify_vocabulary()
+
+    wine_dataset = pd.read_csv("./app/data/test/reduced_wines.csv")
+
+    embeddings_dataframe = construct_taste_ingredient_embeddings()
+
+    avg_taste_embeddings = average_taste_embeddings(embeddings_dataframe)
+
+    wine_df = pd.concat([wine_dataset, embeddings_dataframe], axis=1)
 
 
 if __name__ == "__main__":
